a5315425ad7453e6f7f7f08b7dac57103edf9a5d
Юрий <yuriy.faizov@gmail.com>
2020-09-08 15:23:54 +0300

    getDamage

--- main.js
+++ main.js
@@ -9,10 +9,17 @@ class Game {
         return body;
     }
 
+    getDamage = async () => {
+        const responce = await fetch('https://reactmarathon-api.netlify.app/api/fight?player1id=25&attackId=1&player2id=1');
+        const damage = await responce.json();
+        return damage;
+    }
+
     start = async () => {
         const pokemon = await this.getPokemons();
         const pokemonEnemy = await this.getPokemons();
-
+        const damage = await this.getDamage();
+        
         
         let player1 = new Pokemon({
             ...pokemon,
@@ -28,7 +35,6 @@ class Game {
         const $control = document.querySelector('.control');
 
         player1.attacks.forEach(item => {
-
             const $btn = document.createElement('button');
             $btn.classList.add('button');
             const $span = document.createElement('span');
@@ -38,6 +44,8 @@ class Game {
 
             $btn.addEventListener('click', () => {
                 btnCount();
+
+                console.log(damage)
                 player1.changeHP(random(item.minDamage), function(count) {
                     for (let i = 0; i < 1; i++) {
                         const $logs = document.querySelector('#logs');
@@ -85,73 +93,78 @@ class Game {
 
         function startGame(winner) {   
             if(player1.hp.current <=0 || player2.hp.current <=0 ) {
-
+                newPokemons()
                 const allButtons = document.querySelectorAll('.control .button');   
                 allButtons.forEach($item => $item.remove());
-                const $start = document.querySelector('.control');
-                const $btnstart = document.createElement('button');
-                $btnstart.classList.add('button');
-                $btnstart.innerText = 'Start game!';
-                $start.appendChild($btnstart);
-
-                $btnstart.addEventListener('click', () => {
-                    player1 = new Pokemon({
-                        ...pokemon,
-                        selectors: 'player1',
+                // const $start = document.querySelector('.control');
+                // const $btnstart = document.createElement('button');
+                // $btnstart.classList.add('button');
+                // $btnstart.innerText = 'Start game!';
+                // $start.appendChild($btnstart);
+                winner.remove();
+                // $btnstart.addEventListener('click', () => {
+                //     player1 = new Pokemon({
+                //         ...pokemon,
+                //         selectors: 'player1',
                         
-                    });  
+                //     });  
                     
-                    player2 = new Pokemon({
-                        ...pokemonEnemy,
-                        selectors: 'player2',
+                //     player2 = new Pokemon({
+                //         ...pokemonEnemy,
+                //         selectors: 'player2',
                         
-                    });
+                //     });
+                //     // newPokemons();
 
-                    $btnstart.remove();
-                    winner.remove();
-                    attack()
-                })
+                //     $btnstart.remove();
+                //     winner.remove();
+                //     attack()
+                // })
                 
             } 
             
         }
-        function attack() {
-            player1.attacks.forEach(item => {
-                const $btn = document.createElement('button');
-                $btn.classList.add('button');
-                const $span = document.createElement('span');
-                $span.classList.add('span');
-                $btn.innerText = item.name;
-                const btnCount = countKick(item.maxCount, $btn, $span)
-                $btn.addEventListener('click', () => {
-                    btnCount();
-                    player1.changeHP(random(item.minDamage), function(count) {
-                        for (let i = 0; i < 1; i++) {
-                            const $logs = document.querySelector('#logs');
-                            const $p = document.createElement('p');
-                            $p.innerText = `${generateLog(player2, player1, count, random)}`;
-                            $logs.insertBefore($p, $logs.children[0]);
-            
-                        }
+        // function attack() {
+        //     player1.attacks.forEach(item => {
+        //         const $btn = document.createElement('button');
+        //         $btn.classList.add('button');
+        //         const $span = document.createElement('span');
+        //         $span.classList.add('span');
+        //         $btn.innerText = item.name;
+        //         const btnCount = countKick(item.maxCount, $btn, $span)
+        //         $btn.addEventListener('click', () => {
+        //             btnCount();
+        //             player1.changeHP(random(item.minDamage), function(count) {
                         
-                    }, startGame );
+        //                     const $logs = document.querySelector('#logs');
+        //                     const $p = document.createElement('p');
+        //                     $p.innerText = `${generateLog(player2, player1, count, random)}`;
+        //                     $logs.insertBefore($p, $logs.children[0]);
+            
+                        
+                        
+        //             }, startGame );
                     
             
-                    player2.changeHP(random(item.minDamage), function(count) {
-                        for (let i = 0; i < 1; i++) {
-                            const $logs = document.querySelector('#logs');
-                            const $p = document.createElement('p');
-                            $p.innerText = `${generateLog(player2, player1, count, random)}`;
-                            $logs.insertBefore($p, $logs.children[0]);
-                        }
+        //             player2.changeHP(random(item.minDamage), function(count) {
+                     
+        //                     const $logs = document.querySelector('#logs');
+        //                     const $p = document.createElement('p');
+        //                     $p.innerText = `${generateLog(player2, player1, count, random)}`;
+        //                     $logs.insertBefore($p, $logs.children[0]);
                         
-                    }, startGame );
+                        
+        //             }, startGame );
                     
-                })
+        //         })
                 
-                $control.appendChild($btn);
-                $btn.appendChild($span);
-            })
+        //         $control.appendChild($btn);
+        //         $btn.appendChild($span);
+        //     })
+        // }
+
+        function newPokemons() {
+            game.start();
         }
 
     }
